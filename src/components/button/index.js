b6b64c7b22ee48348f711acbdba71d5cd5339058
Nikolay Fedoseev <n.fedoseev@xsolla.com>
2017-07-06 20:53:06 +0500

    update

--- src/components/button/index.js
+++
@@ -1,7 +0,0 @@
-// class Button extends React.Component {
-//     render() {
-//         return <button>Click</button>;
-//     }
-// }
-//
-// export Button;
--- src/index.js
+++ src/index.js
@@ -5,51 +5,40 @@ import './style.scss'
 // import Button from 'components/button'
 
 class Button extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    handleClick() {
-        console.log(this.props.children);
-    }
-
     render() {
-        return <button onClick={ this.handleClick.bind(this) } >{this.props.children}</button>;
+        return <button onClick={this.props.handler}>{this.props.children}</button>;
     }
 }
 
 class Calculator extends React.Component {
     constructor(props) {
         super(props);
+        this.onBtnClick = this.onBtnClick.bind(this);
         this.state = {number: 10};
     }
 
-    componentDidMount() {
-        console.log(this.fieldEditor1);
+    onBtnClick(number) {
+        console.log(this);
+
+        this.setState({number: number});
     }
 
+    // update(n) {
+    //     this.setState({number: '5'});
+    // }
+
     render() {
+        console.log('this.state.numbe', this.state.number);
         return (
-        <div className="calculator">
-            <div className="screen">{this.state.number}</div>
-            <div className="numberButtons">
-                <div>
-                    <Button ref={(fieldEditor1) => {this.fieldEditor1 = fieldEditor1;}}>1</Button>
-                    <Button>2</Button>
-                    <Button>3</Button>
-                </div>
-                <div>
-                    <Button>4</Button>
-                    <Button>5</Button>
-                    <Button>6</Button>
-                </div>
-                <div>
-                    <Button>7</Button>
-                    <Button>8</Button>
-                    <Button>9</Button>
+            <div className="calculator">
+                <div className="screen">{ this.state.number }</div>
+                <div className="numberButtons">
+                    <div>
+                        <Button handler={ this.onBtnClick(1) } updaetNumber={ this.update(1) }>1</Button>
+                    </div>
                 </div>
             </div>
-        </div>);
+        );
     }
 }
 
