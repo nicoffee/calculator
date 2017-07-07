041908e93402c5b8e0eb43cfc1797dd8539a1fd2
Nikolay Fedoseev <n.fedoseev@xsolla.com>
2017-07-07 16:21:18 +0500

    working prototype

--- src/index.js
+++ src/index.js
@@ -2,42 +2,110 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import './style.scss'
-// import Button from 'components/button'
 
 class Button extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
-        return <button onClick={this.props.handler}>{this.props.children}</button>;
+        return <button onClick={this.props.handler}>{this.props.number}</button>;
     }
 }
 
 class Calculator extends React.Component {
     constructor(props) {
         super(props);
-        this.onBtnClick = this.onBtnClick.bind(this);
-        this.state = {number: 10};
+
+        this.handleClick = this.handleClick.bind(this);
+        this.state = {
+            number: '',
+            pendingOperation: false,
+            operation: ''
+        };
     }
 
-    onBtnClick(number) {
-        console.log(this);
-
-        this.setState({number: number});
+    handleClick(num) {
+        this.setState({
+            number: num
+        });
     }
 
-    // update(n) {
-    //     this.setState({number: '5'});
-    // }
+    handleClickOperation(num1, operator) {
+        const num2 = this.state.number;
+
+        this.setState({
+            pendingOperation: false
+        });
+
+        switch (operator) {
+            case '+':
+                this.setState({
+                    number: num1 + num2,
+                });
+                break;
+            case '-':
+                this.setState({
+                    number: num2 - num1,
+                });
+                break;
+            case '*':
+                this.setState({
+                    number: num1 * num2,
+                });
+                break;
+            case '/':
+                this.setState({
+                    number: num2 / num1,
+                });
+                break;
+        }
+    }
+
+    prepareOperation(operator) {
+        this.setState({
+            pendingOperation: true,
+            operation: operator
+        });
+    }
 
     render() {
-        console.log('this.state.numbe', this.state.number);
         return (
-            <div className="calculator">
-                <div className="screen">{ this.state.number }</div>
-                <div className="numberButtons">
-                    <div>
-                        <Button handler={ this.onBtnClick(1) } updaetNumber={ this.update(1) }>1</Button>
-                    </div>
-                </div>
-            </div>
+          <div className="calculator">
+              <div className="screen">{ this.state.number }</div>
+              <div className="numberButtons">
+                  <div>
+                      <button
+                        value={1}
+                        onClick={
+                            () => {
+                                !this.state.pendingOperation ? this.handleClick(1) : this.handleClickOperation(1, this.state.operation)
+                            }
+                        }>1
+                      </button>
+                      <button
+                        value={2}
+                        onClick={
+                            () => {
+                                !this.state.pendingOperation ? this.handleClick(2) : this.handleClickOperation(2, this.state.operation)
+                            }
+                        }>2
+                      </button>
+                      <button
+                        value={3}
+                        onClick={
+                            () => {
+                                !this.state.pendingOperation ? this.handleClick(3) : this.handleClickOperation(3, this.state.operation)
+                            }
+                        }>3
+                      </button>
+                      <button onClick={() => this.prepareOperation('+')}>+</button>
+                      <button onClick={() => this.prepareOperation('-')}>-</button>
+                      <button onClick={() => this.prepareOperation('*')}>*</button>
+                      <button onClick={() => this.prepareOperation('/')}>/</button>
+                  </div>
+              </div>
+          </div>
         );
     }
 }
