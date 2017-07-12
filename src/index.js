a88c92668a7f1d9de3269288c7bedb89c22b9b13
Nikolay Fedoseev <fedoseevn@gmail.com>
2017-07-12 10:48:57 +0500

    improve

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ class Calculator extends React.Component {
         this.handleClickOperation = this.handleClickOperation.bind(this);
         this.reset = this.reset.bind(this);
         this.calculate = this.calculate.bind(this);
+        this.addDecimal = this.addDecimal.bind(this);
 
         this.state = {
             number: 0,
@@ -19,45 +20,46 @@ class Calculator extends React.Component {
             screen: '0',
             pendingOperation: false,
             pendingInput: false,
-            firstNull: true
+            firstNull: true,
+            isDecimal: false
         };
-
-        this.baseState = this.state;
-
-        console.log('this.baseState', this.baseState);
     }
 
     handleClick(num) {
         const strNum = num.toString();
 
-        if (this.state.firstNull) {
-            console.log('firstNull');
-        this.setState((prevState, props) => {
-            return {
-                number: num,
-                screen: strNum,
-                firstNull: false,
+        console.log(!!this.state.screen);
 
-                    }
-                }
-            )
-        } else {
+        // else {
+
         this.setState((prevState, props) => {
             if (prevState.number) {
+                if (this.state.isDecimal) {
+                    return {
+                        number: prevState.number + num/10,
+                        screen: this.state.screen += strNum,
+                        operation: ''
+                    }
+                }
+
+                console.log('HERR!');
+
                 return {
                     number: prevState.number * 10 + num,
                     screen: this.state.screen += strNum,
-                    operation: this.baseState.operation
+                    operation: ''
                 }
             } else {
+                console.log('THERE!!!!');
+
                 return {
                     number: num,
                     screen: this.state.screen += strNum,
-                    operation: this.baseState.operation
+                    operation: ''
                 }
             }
         });
-        }
+    //}
     }
 
     handleClickOperation(num1, operator) {
@@ -66,17 +68,17 @@ class Calculator extends React.Component {
 
         switch (operator) {
             case '+':
-                result = num2 + num1;
-                break;
+            result = num2 + num1;
+            break;
             case '-':
-                result = num2 - num1;
-                break;
+            result = num2 - num1;
+            break;
             case '*':
-                result = num2 * num1;
-                break;
+            result = num2 * num1;
+            break;
             case '/':
-                result = num2 / num1;
-                break;
+            result = num2 / num1;
+            break;
         }
 
         this.setState({
@@ -89,11 +91,26 @@ class Calculator extends React.Component {
 
     prepareOperation(operator) {
         this.setState((prevState, props) => {
+            console.log('!prevState.screen');
+
+            if (this.state.firstNull && !prevState.screen) {
+                console.log('qweqwe');
+                this.setState((prevState, props) => {
+                    return {
+                        number: num,
+                        screen: strNum,
+                        firstNull: false,
+                    }
+                })
+            }
+
             if (prevState.operation === operator) {
                 return {
                     pendingOperation: true,
                     operation: operator,
-                    number: 0
+                    number: 0,
+                    firstNull: true,
+                    isDecimal: false
                 }
             } else {
                 let string = this.state.screen;
@@ -106,10 +123,21 @@ class Calculator extends React.Component {
                     pendingOperation: true,
                     operation: operator,
                     screen: string + operator,
-                    number: 0
+                    number: 0,
+                    firstNull: true,
+                    isDecimal: false
                 }
             }
         });
+    }
+
+    addDecimal() {
+        if (!this.state.isDecimal) {
+            this.setState({
+                screen: this.state.screen += '.',
+                isDecimal: true
+            })
+        }
     }
 
     reset() {
@@ -118,7 +146,9 @@ class Calculator extends React.Component {
             operation: '',
             screen: '0',
             pendingOperation: false,
-            pendingInput: false
+            pendingInput: false,
+            firstNull: true,
+            isDecimal: false
         })
     }
 
@@ -132,48 +162,49 @@ class Calculator extends React.Component {
         let buttons = [];
         for (let i = 1; i <= 9; i++) {
             buttons.push(
-              <Button
+                <Button
                 key={i}
                 value={i}
                 pending = {this.state.pendingOperation}
                 operation = {this.state.operation}
                 handleClick = {this.handleClick}
                 handleClickOperation = {this.handleClickOperation}
-              />
+                />
             )
         }
 
         return (
-          <div className="calculator">
-              <div className="calculator__screen">{ this.state.screen }</div>
-              <div className="calculator__buttons">
-                  <div className="buttons">
-                      <div className="buttons__numbers">
-                          <Button
-                            value={0}
-                            pending = {this.state.pendingOperation}
-                            operation = {this.state.operation}
-                            handleClick = {this.handleClick}
-                            handleClickOperation = {this.handleClickOperation}
-                          />
-                          {buttons}
-                      </div>
-                      <button onClick={this.reset}>C</button>
-                      <div className="buttons__operations">
-                          <button onClick={() => this.prepareOperation('+')}>+</button>
-                          <button onClick={() => this.prepareOperation('-')}>-</button>
-                          <button onClick={() => this.prepareOperation('*')}>*</button>
-                          <button onClick={() => this.prepareOperation('/')}>/</button>
-                      </div>
-                      <button onClick={this.calculate}>=</button>
-                  </div>
-              </div>
-          </div>
+            <div className="calculator">
+            <div className="calculator__screen">{ this.state.screen }</div>
+            <div className="calculator__buttons">
+            <div className="buttons">
+            <div className="buttons__numbers">
+            <Button
+            value={0}
+            pending = {this.state.pendingOperation}
+            operation = {this.state.operation}
+            handleClick = {this.handleClick}
+            handleClickOperation = {this.handleClickOperation}
+            />
+            {buttons}
+            </div>
+            <button onClick={this.reset}>C</button>
+            <button onClick={this.addDecimal}>.</button>
+            <div className="buttons__operations">
+            <button onClick={() => this.prepareOperation('+')}>+</button>
+            <button onClick={() => this.prepareOperation('-')}>-</button>
+            <button onClick={() => this.prepareOperation('*')}>*</button>
+            <button onClick={() => this.prepareOperation('/')}>/</button>
+            </div>
+            <button onClick={this.calculate}>=</button>
+            </div>
+            </div>
+            </div>
         );
     }
 }
 
 ReactDOM.render(
-  <Calculator />,
-  document.getElementById('root')
+    <Calculator />,
+    document.getElementById('root')
 );
